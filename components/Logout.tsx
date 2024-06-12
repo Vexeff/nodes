16d41923f6dc2d96c6929e707d529ff572e60c62
esfandiar <esfandiar.rouhani@gmail.com>
2024-06-12 00:35:37 -0500

    log out button css added

--- components/Logout.tsx
+++ components/Logout.tsx
@@ -5,7 +5,7 @@ import { logout } from '@/utils/auth_actions'
 export default function Logout(){
     return(
         <div className='flex justify-end w-full mr-5'>
-            <button className='flex p-2 border-neutral-800 border-2 self-end' onClick={() => logout()}>Log out</button>
+            <button className='flex p-2 border-neutral-800 border-2 hover:bg-black hover:duration-300 hover:text-white dark:hover:bg-white dark:text-white dark:hover:text-black self-end' onClick={() => logout()}>Log out</button>
         </div>
     )
 }
