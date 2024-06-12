ff2963765be4ff974c2a56bc1dc0bd1132e08fc6
esfandiar <esfandiar.rouhani@gmail.com>
2024-06-12 00:33:29 -0500

    leaderboard basic fix

--- components/Leaderboard.tsx
+++ components/Leaderboard.tsx
@@ -4,20 +4,20 @@ import { userScore } from "@/utils/types";
 import React from "react";
 import { DataTable } from 'primereact/datatable';
 import { Column } from 'primereact/column';
-import 'primereact/resources/themes/lara-light-indigo/theme.css';
+import 'primereact/resources/themes/tailwind-light/theme.css';
 import 'primereact/resources/primereact.css';
 
 
 export default function LeaderboardTable(  { scores } : { scores: userScore[] } ) {
-    console.log('getting these: ', scores)
+    
     return(
         <div className="flex flex-col text-center justify-center w-full">
             Leaderboard:
-            <div className="flex flex-col my-5 py-2 max-w-full">
-                <div className="z-0 max-w-full">
+            <div className="flex flex-col my-5 py-2 max-w-full ">
+                <div className="z-0 max-w-full ">
                     {scores.length == 0 ? 'Loading...' : 
-                    <DataTable value={scores} tableStyle={{ maxWidth: '60rem', zIndex: 'z-0'}} 
-                    showGridlines stripedRows dataKey="username" selectionMode={'single'} 
+                    <DataTable value={scores} tableStyle={{ minWidth: '20rem', maxWidth: '60rem', zIndex: 'z-0'}} 
+                    showGridlines dataKey="username" selectionMode={'single'} 
                     metaKeySelection={false}
                     scrollable={false}>
                         <Column field="username" header="Username"></Column>
@@ -27,6 +27,6 @@ export default function LeaderboardTable(  { scores } : { scores: userScore[] } 
                 </div>
             </div>
         </div>
-)
+    )
 }
 
